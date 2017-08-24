eaa87557f5576fe4f6d513ebee43fc1781ea31ec
Yuliia <@DESKTOP-6222HOQ>
2017-08-25 01:49:59 +0300

    init commit

--- task4.5/task4.5/Program.cs
+++ task4.5/task4.5/Program.cs
@@ -10,6 +10,25 @@ namespace task4._5
     {
         static void Main(string[] args)
         {
+            int a = 100;
+            int b = 500;
+            int c;
+
+            Console.Write("Enter new price: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (c > b)
+            {
+                Console.WriteLine("The price is higher than 500");
+
+            } else if (c < a)
+
+            {
+                Console.WriteLine("The price is lower than 100");
+
+            } 
+
+            Console.ReadLine();
         }
     }
 }
