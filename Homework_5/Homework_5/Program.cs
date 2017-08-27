dbc6d4adccf8da0c0a274394bc3e4f1893fe5b45
Yuliia <@DESKTOP-6222HOQ>
2017-08-27 16:35:45 +0300

    init commit

---
+++ Homework_5/Homework_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Please, enter a number and we`ll calculate the factorial of your number");
+
+            int n;
+            Console.WriteLine("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+
+            int multiple = n;
+            
+            for (int i = 1; i < n; i++)
+            {
+                multiple = multiple * i;
+            }
+            Console.WriteLine("The result is: " + multiple);
+
+          Console.ReadLine();
+        }
+    }
+}
